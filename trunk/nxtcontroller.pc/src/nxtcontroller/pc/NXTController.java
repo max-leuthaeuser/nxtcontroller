8efeca544382e8438c8cfa54c8a8d79ace56939b
rm.morgenstern <rm.morgenstern@6295cb70-4a83-01fd-5f5a-4f82f0ec2645>
2010-05-18 17:51:12 +0000

    - do all GUI-related work inside the AWT event thread  - little refactoring of NXTController class  - add javadoc to NXTController class  - use spaces instead of tabs

--- trunk/nxtcontroller.pc/src/nxtcontroller/pc/NXTController.java
+++ trunk/nxtcontroller.pc/src/nxtcontroller/pc/NXTController.java
@@ -1,34 +1,42 @@
 package nxtcontroller.pc;
 
-/**
- * @author Max Leuthäuser
- * 
- */
-
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 import nxtcontroller.pc.ui.GUIController;
 
+/**
+ * Main class and application entry point.
+ * 
+ * @author Max Leuthäuser
+ * @author Martin Morgenstern
+ */
 public class NXTController {
-	public static void main(String[] args) {
-		try {
-		    // Set System L&F
-	        UIManager.setLookAndFeel(
-	            UIManager.getSystemLookAndFeelClassName());
-	    } 
-	    catch (UnsupportedLookAndFeelException e) {
-	       // handle exception
-	    }
-	    catch (ClassNotFoundException e) {
-	       // handle exception
-	    }
-	    catch (InstantiationException e) {
-	       // handle exception
-	    }
-	    catch (IllegalAccessException e) {
-	       // handle exception
-	    }
-		GUIController.getInstance().init();
-	}
+    /**
+     * Setup and start the GUI inside the AWT event dispatching thread.
+     * 
+     * @param args
+     *            cmdline args (ignored)
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                setSystemLookAndFeel();
+                GUIController.getInstance().init();
+            }
+        });
+    }
+
+    /**
+     * Try to set the Swing look and feel that is the default for the system.
+     * Exit gracefully.
+     */
+    private static void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            // this exception is intentionally ignored
+        }
+    }
 }
