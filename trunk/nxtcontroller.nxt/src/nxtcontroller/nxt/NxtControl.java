c3d6298d9afebb24d5c20d11a91c9683a1718d30
s7060241@mail.zih.tu-dresden.de <s7060241@mail.zih.tu-dresden.de@6295cb70-4a83-01fd-5f5a-4f82f0ec2645>
2010-05-30 12:43:55 +0000

    - added javadoc

--- trunk/nxtcontroller.nxt/src/nxtcontroller/nxt/NxtControl.java
+++ trunk/nxtcontroller.nxt/src/nxtcontroller/nxt/NxtControl.java
@@ -16,6 +16,14 @@ import lejos.util.Delay;
 import nxtcontroller.shared.CommandId;
 
 // TODO javadoc
+/**
+ * Client side application running on the NXT. Handles incoming commands to
+ * control the direction and power to drive and sends distance values from 3
+ * UltraSonic Sensors and the angle the NXT is rotated to periodically.
+ * 
+ * @author Max Leuthäuser
+ * @author Martin Morgenstern
+ */
 public final class NxtControl implements ButtonListener {
 	private final UltrasonicSensor left = new UltrasonicSensor(SensorPort.S3);
 	private final UltrasonicSensor front = new UltrasonicSensor(SensorPort.S1);
@@ -24,13 +32,37 @@ public final class NxtControl implements ButtonListener {
 	private final Motor motorLeft = Motor.A;
 	private final Motor motorRight = Motor.C;
 
+	/**
+	 * Standard speed at start.
+	 */
 	private int speed = 400;
+	/**
+	 * Maximum speed at the moment (600).
+	 * <p>
+	 * <i>800 is possible with full charged batteries.</i>
+	 * </p>
+	 */
 	private final int MAX_SPEED = 600;
+	/**
+	 * Minimum speed at the moment (200).
+	 */
 	private final int MIN_SPEED = 200;
+	/**
+	 * Value which should be use to increase or decrease the speed of the NXT.
+	 * 
+	 * @see NxtControl#decreaseSpeed()
+	 * @see NxtControl#increaseSpeed()
+	 */
 	private final int SPEED_INC_VALUE = 50;
 	private final int ROTATE_VALUE = 200;
 
+	/**
+	 * Distance between the two wheels of the NXT.
+	 */
 	private final float trackWidth = 65.0f;
+	/**
+	 * Diameter of the wheels which are used.
+	 */
 	private final float wheelDiameter = 52.0f;
 
 	private final OutputStream out;
@@ -38,7 +70,14 @@ public final class NxtControl implements ButtonListener {
 
 	private final SenderThread senderThread = new SenderThread();
 
-	// TODO fail-safe bluetooth connection
+	/**
+	 * Waits for an incoming bluetooth connection at start and retrieves its
+	 * input- and outputstream.
+	 * 
+	 * <p>
+	 * TODO: fail-safe bluetooth connection
+	 * </p>
+	 */
 	private NxtControl() {
 		Button.ESCAPE.addButtonListener(this);
 
@@ -51,69 +90,117 @@ public final class NxtControl implements ButtonListener {
 
 	}
 
+	/**
+	 * Starts sending the sensor values and dispatches incoming commands.
+	 * 
+	 * @see SenderThread
+	 * @see NxtControl#dispatchCommands()
+	 */
 	public void start() {
 		senderThread.start();
 		dispatchCommands();
 	}
 
+	/**
+	 * NXT moves backward continuously. To stop the NXT use
+	 * {@link NxtControl#stop()}
+	 */
 	private void backward() {
 		normalize();
 		motorLeft.backward();
 		motorRight.backward();
 	}
 
+	/**
+	 * NXT moves forward continuously. To stop the NXT use
+	 * {@link NxtControl#stop()}
+	 */
 	private void forward() {
 		normalize();
 		motorLeft.forward();
 		motorRight.forward();
 	}
 
+	/**
+	 * NXT turns left on the spot continuously. To stop the NXT use
+	 * {@link NxtControl#stop()}
+	 */
 	private void left() {
 		normalize();
 		motorLeft.forward();
 		motorRight.backward();
 	}
 
+	/**
+	 * NXT turns right on the spot continuously. To stop the NXT use
+	 * {@link NxtControl#stop()}
+	 */
 	private void right() {
 		normalize();
 		motorLeft.backward();
 		motorRight.forward();
 	}
 
+	/**
+	 * NXT moves along a curve forward and left continuously. To stop the NXT
+	 * use {@link NxtControl#stop()}
+	 */
 	private void forwardLeft() {
 		motorLeft.setSpeed(speed + ROTATE_VALUE);
 		motorLeft.forward();
 		motorRight.forward();
 	}
 
+	/**
+	 * NXT moves along a curve forward and right continuously. To stop the NXT
+	 * use {@link NxtControl#stop()}
+	 */
 	private void forwardRight() {
 		motorRight.setSpeed(speed + ROTATE_VALUE);
 		motorLeft.forward();
 		motorRight.forward();
 	}
 
+	/**
+	 * NXT moves along a curve backward and left continuously. To stop the NXT
+	 * use {@link NxtControl#stop()}
+	 */
 	private void backwardLeft() {
 		motorLeft.setSpeed(speed + ROTATE_VALUE);
 		motorLeft.backward();
 		motorRight.backward();
 	}
 
+	/**
+	 * NXT moves along a curve backward and right continuously. To stop the NXT
+	 * use {@link NxtControl#stop()}
+	 */
 	private void backwardRight() {
 		motorRight.setSpeed(speed + ROTATE_VALUE);
 		motorLeft.backward();
 		motorRight.backward();
 	}
 
+	/**
+	 * Stop the NXT immediately.
+	 */
 	private void stop() {
 		motorLeft.stop();
 		motorRight.stop();
 	}
 
+	/**
+	 * Recover the base speed.
+	 */
 	private void normalize() {
 		motorLeft.setSpeed(speed);
 		motorRight.setSpeed(speed);
 	}
 
+	/**
+	 * Decrease the speed between {@link NxtControl#MAX_SPEED} and
+	 * {@link NxtControl#MIN_SPEED} using {@link NxtControl#SPEED_INC_VALUE}
+	 */
 	private void decreaseSpeed() {
 		if (speed >= MIN_SPEED) {
 			speed -= SPEED_INC_VALUE;
@@ -122,6 +209,10 @@ public final class NxtControl implements ButtonListener {
 		motorRight.setSpeed(speed);
 	}
 
+	/**
+	 * Increase the speed between {@link NxtControl#MAX_SPEED} and
+	 * {@link NxtControl#MIN_SPEED} using {@link NxtControl#SPEED_INC_VALUE}
+	 */
 	private void increaseSpeed() {
 		if (speed <= MAX_SPEED) {
 			speed += SPEED_INC_VALUE;
@@ -130,6 +221,12 @@ public final class NxtControl implements ButtonListener {
 		motorRight.setSpeed(speed);
 	}
 
+	/**
+	 * Check commands via bluetooth continuously an decide which move should be
+	 * performed.
+	 * 
+	 * @see NxtControl#receive()
+	 */
 	private void dispatchCommands() {
 		for (;;) {
 			switch (receive()) {
@@ -173,6 +270,9 @@ public final class NxtControl implements ButtonListener {
 		}
 	}
 
+	/**
+	 * @return the current command from the bluetooth input stream.
+	 */
 	private int receive() {
 		int result;
 
@@ -185,6 +285,9 @@ public final class NxtControl implements ButtonListener {
 		return result;
 	}
 
+	/**
+	 * Shutdown the NXT and close all bluetooth streams.
+	 */
 	public void shutdown() {
 		stop();
 
@@ -199,16 +302,30 @@ public final class NxtControl implements ButtonListener {
 	}
 
 	/**
+	 * Entry point of the application.
+	 * 
 	 * @param args
+	 *            ignored here
 	 */
 	public static void main(String[] args) {
 		NxtControl nxt = new NxtControl();
 		nxt.start();
 	}
 
+	/**
+	 * Thread which sends UltraSonic Sensor values and the current
+	 * rotation value via bluetooth continuously.
+	 */
 	private final class SenderThread extends Thread {
 		private static final int DELAY = 50;
 
+		/**
+		 * Start this thread and send values continuously.
+		 * Uses {@link SenderThread#DELAY} to wait between sending a set of new values.
+		 * 
+		 * @see SenderThread#sendValues()
+		 * @see java.lang.Thread#run()
+		 */
 		@Override
 		public void run() {
 			for (;;) {
@@ -217,12 +334,23 @@ public final class NxtControl implements ButtonListener {
 			}
 		}
 
+		/**
+		 * @return the angle the NXT is rotated to.
+		 * 
+		 * @see NxtControl#trackWidth
+		 * @see NxtControl#wheelDiameter
+		 */
 		private float getAngle() {
 			float turnRatio = trackWidth / wheelDiameter;
 			return ((motorRight.getTachoCount() / turnRatio) - (motorLeft
 					.getTachoCount() / turnRatio)) / 2.0f;
 		}
 
+		/**
+		 * Sends a set of UltraSonic sensor values and the current angle.
+		 * 
+		 * @see SenderThread#getAngle()
+		 */
 		private void sendValues() {
 			try {
 				out.write(left.getDistance());
